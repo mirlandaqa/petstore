fc4c7a7dec0c13e754175bafc50b67da876393ad
Mirlanda Costa <mirlanda.dsc@gmail.com>
2022-11-24 01:24:28 -0400

    Adicionando estrutura de projeto de automação de testes de API

---
+++ src/test/java/modulos/animal/AnimalTest.java
@@ -0,0 +1,14 @@
+package modulos.animal;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Testes de API Rest do modulo A")
+public class AnimalTest {
+
+    @Test
+    @DisplayName("Pesquisar um pet por ID")
+    public void testValidarBuscaDePetPorID(){
+
+    }
+}
